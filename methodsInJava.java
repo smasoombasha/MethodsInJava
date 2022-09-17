1cf16c7e9680cf75abddbe147b1f3248ceb32a83
S Masoom Basha <aahens.m143@gmail.com>
2022-09-17 19:52:09 +0530

    initial commit

--- methodsInJava.java
+++ methodsInJava.java
@@ -7,6 +7,10 @@ public class methodsInJava {
 
         calculateFinalScore(true, 3000, 200, 100);
         calculateFinalScore2(true, 4000, 250, 2);
+        // One more type in Methods
+        int FinalScore4 = calculateFinalScore3(true, 5000, 400, 3);
+            System.out.println("Your FinalScore is "+FinalScore4);
+
     }
     // Creating Method with void(returns nothing) keyword
     public static void calculateFinalScore(boolean gameOver, int score, int levelCompleted, int bonus ){
@@ -28,5 +32,14 @@ public class methodsInJava {
         }else{
             return -1;
         }
+    }
+    public static int calculateFinalScore3(boolean gameOver, int score, int levelCompleted, int bonus){
+        if(gameOver){
+            int finalScore = score +(levelCompleted*bonus);
+            System.out.println("Your FinalScore is "+finalScore);
+            return finalScore;
+        }
+        return -1;
+
     }
 }
