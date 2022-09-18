294b9cf3680ca9be318088406361a9465e51d05e
S Masoom Basha <aahens.m143@gmail.com>
2022-09-18 11:16:33 +0530

    initial commit

--- MethodChallenge.java
+++ MethodChallenge.java
@@ -27,14 +27,23 @@ public class MethodChallenge {
                 System.out.println(displayHighScore);
     }
     public static int calculateHighScorePosition(int score){
-      if(score >=1000){
-          return 1;
-      }else if(score >=500){
-          return 2;
-      }else if(score >=100){
-          return 3;
-      }else{
-          return 4;
-      }
+     // if(score >=1000){
+     //     return 1;
+     // }else if(score >=500){
+     //     return 2;
+     // }else if(score >=100){
+     //     return 3;
+     // }else{
+     //     return 4;
+     // }
+        int position = 4;
+        if(score >= 1000){
+            position =1;
+        }else if(score >=500){
+            position = 2;
+        }else if(score >= 100){
+            position =3;
+        }
+        return position;
     }
 }
